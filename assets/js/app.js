94868d23783d16a7ec699f40517e3ef6aa71adc6
AndyEm <andyem64@gmail.com>
2017-03-19 18:22:06 -0400

    added info-box. Countinues to next section when user double clicks after making initial pick

--- assets/js/app.js
+++ assets/js/app.js
@@ -109,38 +109,35 @@ function getVideoDetails(id) {
 
 //Where the map is viewed when page loads 
 var startDisp = {lat: 0, lng: 0};
-
-var long;
-var lad;
+var getMap = document.getElementById('map');
 var marker;
 var randomMarker;
-var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-//Model box
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-function initMap() {
-
-    var map = new google.maps.Map(document.getElementById('map'), {
+var questionCounter = 0;
+var options1 = {
         //Zooms in or out on the map
         zoom: 1,
         //Displays map starting at certain location
         center: startDisp,
-         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#c9323b"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#c9323b"},{"weight":1.2}]},{"featureType":"administrative.locality","elementType":"geometry.fill","stylers":[{"lightness":"-1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"lightness":"0"},{"saturation":"0"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.stroke","stylers":[{"weight":"0.01"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.stroke","stylers":[{"weight":"0.01"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#c9323b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#99282f"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#99282f"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#090228"}]}]
-    });
+        styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#c9323b"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#c9323b"},{"weight":1.2}]},{"featureType":"administrative.locality","elementType":"geometry.fill","stylers":[{"lightness":"-1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"lightness":"0"},{"saturation":"0"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.stroke","stylers":[{"weight":"0.01"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.stroke","stylers":[{"weight":"0.01"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#c9323b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#99282f"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#99282f"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#99282f"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#090228"}]}]
+    };
+//Model box
+//===========================
+//Counts the number of questions 
+function initMap() {
+    //Displays initial map
+    var map = new google.maps.Map(getMap,options1);
 
     // When area on map is clicked......
     google.maps.event.addListener(map,'click',function(event) {
+        
+        //Prevents the map from being clicked a second time after choosing location
+        map = new google.maps.Map(getMap,options1);
         //Displays marker on map depending on users longitude/latitude
         randomMarker = new google.maps.Marker({
             //Puts marker at location basec on "randomLongLat" value
             position: city,
             map: map,
+            icon:'http://maps.google.com/mapfiles/kml/paddle/red-stars.png'
         });
 
         //Gets the latitude and longitude
@@ -171,40 +168,41 @@ function initMap() {
             strokeWeight: 3,
             map: map
         });
-
-
-        //Model Box
-        //========================================================
+       
         // When the user clicks on the button, open the modal 
             var latInfo = city.lat;
             var longInfo = city.lng;
             $.ajax({
                 url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latInfo + ',' + longInfo + '&sensor=true',
                 method: 'GET'
-            }).done(function(response){
-                $('.modelText').html('The area is ' + response['results'][0]['formatted_address'] +
-                                      'and you were ' + Math.round(distance) + ' miles off');
-                //Testing
-                console.log('The area is ' + response['results'][0]['formatted_address']);
+            })
+            .done(function(response){
+                //Displays infobox above random marker
+                var contentString = 'The area is ' + response['results'][0]['formatted_address'] +
+                                          ' and you were ' + Math.round(distance) + ' miles off' + 
+                                          '<br>' + '<br>' + 
+                                          'Double-Click anywhere to continue';
+                var infowindow = new google.maps.InfoWindow({
+                  content: contentString,
+                  maxWidth:200
+                });
+                //displays info box on marker
+                infowindow.open(map,randomMarker);
+                // When map is clicked after info box is displayed it goes to the next question
+                google.maps.event.addListener(map, 'dblclick', function() {
+                    if(infowindow){
+                       //adds 1 to question counter
+                       questionCounter++;
+                       //Closes info box above marker
+                       infowindow.close();
+                       //goes to the next question
+                       nextQuestion();
+                    }
+                });
+
             });
-            modal.style.display = "block";
-
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-            nextQuestion();
-        }
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-                nextQuestion();
-            }
-        }
     });
-
-            
+         
 }
 //Function to create marker
 function placeMarkerAndPanTo(latLng, map) {
@@ -212,10 +210,19 @@ function placeMarkerAndPanTo(latLng, map) {
         position: latLng,
         map: map
     });
+
 }
 function nextQuestion(){
+    //Resets long/lat
     city = locationArray[Math.floor(Math.random() * locationArray.length)];
-    initMap();
+    //Calls function to jump to the next question
+    console.log('question number ' + questionCounter);
+    if(questionCounter < 4){
+        initMap();
+    }
+    else{
+        alert('Game Over');
+    }
 }
 
 
