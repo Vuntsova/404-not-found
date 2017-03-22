87791b3ff8402fc6c2b9d140b349a55f8a8d9a74
ranghel <anghelramona30@gmail.com>
2017-03-22 10:05:35 -0400

    fixed firebasejs to previous version

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,19 +1,18 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyBXDYThOZ40w_73kgIsACKsv8_4fqOFl-w",
     authDomain: "video-game-2a8aa.firebaseapp.com",
     databaseURL: "https://video-game-2a8aa.firebaseio.com",
     storageBucket: "video-game-2a8aa.appspot.com",
     messagingSenderId: "714221763835"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var dataRef = firebase.database();
-//
-// // Initial Values
-// var username = "";
-// var emai = "";
-// var score = 0;
+
+// Initial Values
+var username = "";
+var score = 0;
 
 // Caputure Submit Click
 $("#playBtn").on("click", function() {
@@ -23,21 +22,26 @@ $("#playBtn").on("click", function() {
     var userName = $("#username").val().trim();
     // var score -- need to figure it out
 
+
     // Create a new object and push into db
     var players = {
         username: userName,
-        
         //score: score
     }
 
     // Code for pushing to db
-    dataRef.ref().push(players);
+    if (userName != "" ) {
+        database.ref().push(players);
+    } else {
+        alert("Please enter train data");
+    }
+
 
     // Clear out form fields
-    $("input").val("");
+    $("#username").val("");
 
     console.log("Username: " + players.username);
-    //console.log("Email: " + players.email);
+    //console.log("Score: " + players.score);
 
 
 });
@@ -49,14 +53,9 @@ dataRef.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
 
     var userName = childSnapshot.val().username;
-    //var email = childSnapshot.val().email;
+    //var score = childSnapshot.val().score;
 
     //$("#leader-board > tbody").append("<tr><td>" + userName + "</td><td" + email);
 }, function(errorObject){
     console.log("Errors handled: " + errorObject.code);
-<<<<<<< HEAD
-});
 
-=======
-});
->>>>>>> dde03471092e9b5c815f456c7006a28c97182cde
